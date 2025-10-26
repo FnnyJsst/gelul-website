cca9e0fcde3c098891302bb5fd722850c4d89611
FnnyJsst <fanny.jousset@gmail.com>
2025-10-26 12:32:47 +0100

    style: product banner

---
+++ vite-project/src/components/banners/ProductBanner.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { fontSizes } from '../../constants/style'
+import { colors } from '../../constants/style'
+import styled from 'styled-components'
+
+const BannerDiv = styled.div`
+  position: relative;
+  height: 10vh;
+  width: 100%;
+  padding-left: 10vh;
+  background-image: url("${new URL('../../assets/images/blue-plate.jpeg', import.meta.url).href}");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+`;
+
+const BreadcrumbContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  z-index: 2;
+`;
+
+const BreadcrumbLink = styled.button`
+  background: none;
+  border: none;
+  color: ${colors.white};
+  font-size: ${fontSizes.medium};
+  text-decoration: none;
+  transition: opacity 0.3s ease;
+  cursor: pointer;
+  
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Separator = styled.span`
+  color: ${colors.white};
+  font-size: ${fontSizes.mediumTitle};
+  opacity: 0.8;
+`;
+
+function ProductBanner() {
+  const navigate = useNavigate()
+
+  const handleBoutiqueClick = () => {
+    navigate('/boutique')
+  }
+
+  return (
+    <BannerDiv>
+      <BreadcrumbContainer>
+        <BreadcrumbLink onClick={handleBoutiqueClick}>Boutique</BreadcrumbLink>
+        <Separator>/</Separator>
+        <BreadcrumbLink>Produit</BreadcrumbLink>
+      </BreadcrumbContainer>
+      <Overlay />
+    </BannerDiv>
+  )
+}
+
+export default ProductBanner
--- vite-project/src/pages/ProductPage.jsx
+++ vite-project/src/pages/ProductPage.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import ProductBanner from '../components/banners/ProductBanner'
+import styled from 'styled-components'
+
+const ContentContainer = styled.div`
+  min-height: 50vh;
+  padding: 5rem;
+`
 
 function ProductPage() {
   const { id } = useParams()
 
   return (
-    <div>
-      <h1>Page Produit #{id}</h1>
-      {/* Vous pourrez ajouter ici le contenu de la page produit */}
-    </div>
+    <>
+      <ProductBanner />
+      <ContentContainer>
+        <img src={product.image} alt={product.name} />
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+        <button>Ajouter au panier</button>
+        <button>Ajouter aux favoris</button>
+        <button>Ajouter à la liste de souhaits</button>
+        <button>Ajouter à la liste de souhaits</button>
+      </ContentContainer>
+    </>
   )
 }
 
