4c2fa129530dae788851f0813b8825450aa38312
FnnyJsst <fanny.jousset@gmail.com>
2025-12-13 09:07:58 +0100

    back to coming soon mode

--- vite-project/src/config/comingSoon.js
+++ vite-project/src/config/comingSoon.js
@@ -1,5 +1,5 @@
 // Configuration pour le mode "Site en construction"
 // Mettre à true pour afficher la page "en construction"
 // Mettre à false pour afficher le site complet
-export const IS_COMING_SOON_MODE = false;
+export const IS_COMING_SOON_MODE = true;
 
